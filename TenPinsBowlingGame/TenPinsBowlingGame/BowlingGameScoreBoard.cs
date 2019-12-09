f94a5cd2f43960bd96eb7d122ead03ac1599e8ce
Luke-iq <lukel@iqmetrix.com>
2019-12-09 11:45:55 -0800

    Day 4: Pass all tests

--- TenPinsBowlingGame/TenPinsBowlingGame/BowlingGameScoreBoard.cs
+++ TenPinsBowlingGame/TenPinsBowlingGame/BowlingGameScoreBoard.cs
@@ -12,7 +12,13 @@ namespace TenPinsBowlingGame
         private const char Spare = '/';
         private const int StrikeBonusCount = 2;
         private const int SpareBonusCount = 1;
+        private const int totalNumberOfFrame = 10;
+        private const int bonusFrame = 11;
+        private const int frameTenIndex = 9;
+        private const int firstBallOfAFrame = 0;
+        private const int secondBallOfAFrame = 1;
 
+        private int numberOfBonus = 0;
         private int totalScore = 0;
 
         private string[] ParseGameInfo(string gameInfo)
@@ -21,6 +27,14 @@ namespace TenPinsBowlingGame
             if (gameScoreBoard.Length != 12)
             {
                 return null;
+            }
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (gameScoreBoard[frame].Length == 0)
+                    throw new InvalidGameInputException($"Invalid game input {gameInfo} without data for a frames.");
+                if (gameScoreBoard[frame].Length > 2)
+                    throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
             }
 
             return gameScoreBoard;
@@ -74,7 +88,7 @@ namespace TenPinsBowlingGame
                 {
                     bonusRecords.Enqueue(new int[] { bonusDetail[0], 1 });
                 }
-
+                numberOfBonus--;
                 bonusScore += currentPins;
             }
 
@@ -83,13 +97,8 @@ namespace TenPinsBowlingGame
 
         public int ComputeTenPinsBowlingGameScores(string gameInfo)
         {
-            const int totalNumberOfFrame = 10;
-            const int bonusFeame = 11;
-            const int firstBallOfAFrame = 0;
-            const int secondBallOfAFrame = 1;
-
             Queue bonusBallRecords = new Queue();
-        
+            
             totalScore = 0;
 
             string[] gameScoreBoard = ParseGameInfo(gameInfo);
@@ -98,12 +107,13 @@ namespace TenPinsBowlingGame
                 throw new InvalidGameInputException($"Invalid game input {gameInfo}");
             }
 
+            char frameTenDetail = '_';
             for(int currentFrame = 0; currentFrame < totalNumberOfFrame; currentFrame++)
             {
                 char firstBall = char.ToLower(gameScoreBoard[currentFrame][firstBallOfAFrame]);
                 if (!isValidFirstBall(firstBall))
                 {
-                    throw new InvalidGameInputException($"Invalid game input {gameInfo}");
+                    throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
                 }
 
                 var pinsDroppedForFirstBall = convertSingleScoreRecordToInt(firstBall);
@@ -112,52 +122,98 @@ namespace TenPinsBowlingGame
 
                 if (firstBall == Strike)
                 {
+                    if (currentFrame == frameTenIndex)
+                    {
+                        frameTenDetail = Strike;
+                    }
+                    numberOfBonus += 2;
                     bonusBallRecords.Enqueue(new int[] { currentFrame, StrikeBonusCount });
                 }
 
                 if (gameScoreBoard[currentFrame].Length == 2)
                 {
+                    if(firstBall == Strike)
+                    {
+                        throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
+                    }
+
                     char secondBall = char.ToLower(gameScoreBoard[currentFrame][secondBallOfAFrame]);
                     if (!isValidSecondBall(secondBall))
                     {
-                        throw new InvalidGameInputException($"Invalid game input {gameInfo}");
+                        throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
                     }
 
+                    int pinsDroppedForSecondBall = 0;
                     if (secondBall == Spare)
                     {
-                        int sparePins = 10 - convertSingleScoreRecordToInt(firstBall);
-                        totalScore += (sparePins);
-                        totalScore += computeBonusScore(bonusBallRecords, sparePins);
+                        if (currentFrame == frameTenIndex)
+                        {
+                            frameTenDetail = Spare;
+                        }
+                        pinsDroppedForSecondBall = 10 - convertSingleScoreRecordToInt(firstBall);
 
+                        totalScore += pinsDroppedForSecondBall;
+                        totalScore += computeBonusScore(bonusBallRecords, pinsDroppedForSecondBall);
+
+                        numberOfBonus++;
                         bonusBallRecords.Enqueue(new int[] {currentFrame, SpareBonusCount});
                     }
                     else
                     {
-                        int pinsDroppedForSecondBall = convertSingleScoreRecordToInt(secondBall);
+                        pinsDroppedForSecondBall = convertSingleScoreRecordToInt(secondBall);
+
+                        if (pinsDroppedForFirstBall + pinsDroppedForSecondBall > 10)
+                        {
+                            throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
+                        }
+
                         totalScore += pinsDroppedForSecondBall;
                         totalScore += computeBonusScore(bonusBallRecords, pinsDroppedForSecondBall);
                     }
                 }
             }
 
-            if (gameScoreBoard[bonusFeame] != null && bonusBallRecords.Count > 0)
+
+            if (frameTenDetail == Strike && gameScoreBoard[bonusFrame].Length != 2)
             {
-                char firstBall = char.ToLower(gameScoreBoard[bonusFeame][firstBallOfAFrame]);
+                throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
+            }
+
+            if (frameTenDetail == Spare && gameScoreBoard[bonusFrame].Length != 1)
+            {
+                throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
+            }
+
+
+            if (frameTenDetail != Strike && frameTenDetail != Spare && gameScoreBoard[bonusFrame].Length != 0)
+            {
+                throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
+            }
+
+
+            if (bonusBallRecords.Count > 0)
+            {
+                //if (numberOfBonus != gameScoreBoard[bonusFrame].Length)
+                //{
+                //    throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
+                //}
+
+                char firstBall = char.ToLower(gameScoreBoard[bonusFrame][firstBallOfAFrame]);
                 if (!isValidFirstBall(firstBall))
                 {
-                    throw new InvalidGameInputException($"Invalid game input {gameInfo}");
+                    throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
                 }
 
                 var pinsDroppedForFirstBall = convertSingleScoreRecordToInt(firstBall);
 
                 totalScore += computeBonusScore(bonusBallRecords, pinsDroppedForFirstBall);
 
-                if (gameScoreBoard[bonusFeame].Length == 2)
+                if (gameScoreBoard[bonusFrame].Length == 2)
                 {
-                    char secondBall = char.ToLower(gameScoreBoard[bonusFeame][secondBallOfAFrame]);
+                    char secondBall = char.ToLower(gameScoreBoard[bonusFrame][secondBallOfAFrame]);
                     if (!isValidSecondBonusBall(secondBall))
                     {
-                        throw new InvalidGameInputException($"Invalid game input {gameInfo}");
+                        throw new InvalidGameInputException($"Invalid game input {gameInfo} with invalid data for a frames.");
                     }
 
                     if (secondBall == Spare)
@@ -170,11 +226,10 @@ namespace TenPinsBowlingGame
                     else
                     {
                         int pinsDroppedForSecondBall = convertSingleScoreRecordToInt(secondBall);
-                        
+
                         totalScore += computeBonusScore(bonusBallRecords, pinsDroppedForSecondBall);
                     }
                 }
-
             }
 
             return totalScore;

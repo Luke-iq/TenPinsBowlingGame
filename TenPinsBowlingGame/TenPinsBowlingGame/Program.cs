3087e8759aaee0f7e0b842b8c423eaa62cdba225
Luke-iq <lukel@iqmetrix.com>
2019-12-15 20:03:26 -0800

    QuickAndDirty soltuion with all test inputs in console sample.

--- TenPinsBowlingGame/TenPinsBowlingGame/Program.cs
+++ TenPinsBowlingGame/TenPinsBowlingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TenPinsBowlingGame.ExceptionHandlers;
 
 namespace TenPinsBowlingGame
 {
@@ -6,21 +7,50 @@ namespace TenPinsBowlingGame
     {
         static void Main(string[] args)
         {
-            const string bowlingGameStats = "X|X|21|--|--|--|--|--|--|--||";
-            char input = '_';
+            var bowlingGameStats = new[]
+            {
+                "11|22|33|44|11|22|33|44|11|22||",
+                "X|X|21|--|--|--|--|--|--|--||",
+                "5/|21|--|--|--|--|--|--|--|--||",
+                "--|--|--|--|--|--|--|--|--|X||22",
+                "--|--|--|--|--|--|--|--|--|8/||2",
+                "X|X|X|X|X|X|X|X|X|X||XX",
+                "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5",
+                "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||",
+                "X|7/|9-|X|-8|8/|-6|X|X|X||81"
+            };
+
             BowlingGameScoreBoard bowlingGameScoreBoard = new BowlingGameScoreBoard();
 
-            while (input != 'x')
+            Console.WriteLine("Bowling Score Board");
+
+            foreach (var gameInput in bowlingGameStats)
             {
-                Console.WriteLine("Bowling Score Board");
+                var totalScore = bowlingGameScoreBoard.ComputeTenPinsBowlingGameScores(gameInput);
 
-                Console.WriteLine(bowlingGameStats);
+                Console.WriteLine($"{gameInput}\n\t\t\t\t\t{totalScore}\n");
+            }
 
-                var totalScore = bowlingGameScoreBoard.ComputeTenPinsBowlingGameScores(bowlingGameStats);
+            Console.WriteLine("Please Enter a string for bowling game or `q` to end program:");
+            var input = Console.ReadLine();
 
-                Console.WriteLine($"  |  |  |  |  |  |  |  |  |  ||  {totalScore}");
+            while (input != "q")
+            {
 
-                input = char.ToLower(Console.ReadKey().KeyChar);
+                try
+                {
+                    var totalScore = bowlingGameScoreBoard.ComputeTenPinsBowlingGameScores(input);
+
+                    Console.WriteLine($"\t\t\t\t\t{totalScore}\n");
+
+                }
+                catch (InvalidGameInputException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Please Enter a string for bowling game or 'q' to end program:");
+                input = Console.ReadLine();
             }
         }
     }
